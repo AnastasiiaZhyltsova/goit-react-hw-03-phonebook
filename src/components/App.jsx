a92fbe64b9f20fa983d6d45e93d18e18762b15e7
AnastasiiaZhyltsova <anastasiiazhyltsova@gmail.com>
2022-07-13 17:58:09 +0300

    localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,93 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+
+import React, { Component } from "react";
+import { nanoid } from "nanoid";
+
+import Form from "./Form";
+import Filter from "./Filter";
+import ContactList from "./ContactList";
+
+class App extends Component {
+state = {
+  contacts: [],
+  filter:'',
+  }
+  // компонент замаунтился(первый раз, 1 раз) при обновлении стр 
+  componentDidMount(){
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    console.log(parsedContacts);
+    if (parsedContacts) {
+          this.setState({contacts: parsedContacts})
+
+    }
+}  
+// метод жизненного цикла Вызывается сразу после обновления компонента в DOM
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      console.log('Обновилось')
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+  
+  formSubmitHandler = ({name, number}) => {
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    }
+  
+    const { contacts } = this.state;
+    const normalzeName = name.toLocaleLowerCase();
+   // eslint-disable-next-line
+    {contacts.find(contact => contact.name.toLocaleLowerCase() === normalzeName)   
+       ? alert(`${contact.name} is already in contacts`)
+       : this.setState(({contacts}) => ({contacts: [contact, ...contacts]}))
+  }  
+  };
+
+  deleteContact = (contactId) => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }))
+  }
+   
+  changeFilter = e => {
+    this.setState({
+      filter: e.currentTarget.value
+    })
+  };
+  
+  getContactsFilter = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLocaleLowerCase();
+    return contacts.filter(contact => contact.name.toLocaleLowerCase().includes(normalizedFilter))
+  }
+ 
+
+  render() {
+    const { contacts, filter } = this.state;
+    const contactsFilter = this.getContactsFilter();
+    return (
+      <div> 
+         <h1>Phonebook</h1>  
+        <Form
+          onSubmit={this.formSubmitHandler}
+          contacts = {contacts}
+        /> 
+
+        <h2>Contacts</h2>  
+        <Filter
+          value={filter}
+          onChange={this.changeFilter}         
+        />
+        <ContactList
+          contacts={contactsFilter}
+          onDeleteContact={this.deleteContact}
+        />         
+      </div>  
+    );
+  }
+}
+
+export default App;
